28628f0813fca2ae2e5c2d7a7ebd425f6535dc75
Andrey Rybalko <root.andriell@gmail.com>
2025-09-25 17:02:29 +0300

    init

---
+++ src/helper/RelativeDateHelper.test.ts
@@ -0,0 +1,96 @@
+import { RelativeDateHelper } from './RelativeDateHelper';
+
+test('RelativeDateHelper.tz', () => {
+  const x = new Date();
+  expect(x.getTimezoneOffset()).toEqual(-180);
+})
+
+test('RelativeDateHelper.date', () => {
+  //       2020-05
+  // пн вт ср чт пт сб вс
+  //              1  2  3
+  //  4  5  6  7  8  9 10
+  // 11 12 13 14 15 16 17
+  // 18 19 20 21 22 23 24
+  // 25 26 27 28 29 30 31
+  const date = new Date('2020-05-15T16:32:45.248Z');
+
+  expect(RelativeDateHelper.date(undefined, { date })).toStrictEqual(new Date('2020-05-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date(undefined, { date, withMs: true })).toStrictEqual(new Date('2020-05-15T16:32:45.248Z'));
+  expect(RelativeDateHelper.date({}, { date })).toStrictEqual(new Date('2020-05-15T16:32:45.000Z'));
+
+  expect(RelativeDateHelper.date({ y: '1900' }, { date })).toStrictEqual(new Date('1900-05-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ y: '+5' }, { date })).toStrictEqual(new Date('2025-05-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ y: '-5' }, { date })).toStrictEqual(new Date('2015-05-15T16:32:45.000Z'));
+
+  expect(RelativeDateHelper.date({ m: '1' }, { date })).toStrictEqual(new Date('2020-01-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ m: '+5' }, { date })).toStrictEqual(new Date('2020-10-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ m: '-5' }, { date })).toStrictEqual(new Date('2019-12-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ m: '+12' }, { date })).toStrictEqual(new Date('2021-05-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ m: '-12' }, { date })).toStrictEqual(new Date('2019-05-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ m: '+17' }, { date })).toStrictEqual(new Date('2021-10-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ m: '-17' }, { date })).toStrictEqual(new Date('2018-12-15T16:32:45.000Z'));
+
+  expect(RelativeDateHelper.date({ d: '1' }, { date })).toStrictEqual(new Date('2020-05-01T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ d: '+5' }, { date })).toStrictEqual(new Date('2020-05-20T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ d: '-5' }, { date })).toStrictEqual(new Date('2020-05-10T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ d: '+31' }, { date })).toStrictEqual(new Date('2020-06-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ d: '-30' }, { date })).toStrictEqual(new Date('2020-04-15T16:32:45.000Z'));
+
+  expect(RelativeDateHelper.date({ h: '3' }, { date })).toStrictEqual(new Date('2020-05-15T00:32:45.000Z'));
+  expect(RelativeDateHelper.date({ h: '+5' }, { date })).toStrictEqual(new Date('2020-05-15T21:32:45.000Z'));
+  expect(RelativeDateHelper.date({ h: '-5' }, { date })).toStrictEqual(new Date('2020-05-15T11:32:45.000Z'));
+  expect(RelativeDateHelper.date({ h: '+25' }, { date })).toStrictEqual(new Date('2020-05-16T17:32:45.000Z'));
+  expect(RelativeDateHelper.date({ h: '-25' }, { date })).toStrictEqual(new Date('2020-05-14T15:32:45.000Z'));
+
+  expect(RelativeDateHelper.date({ i: '1' }, { date })).toStrictEqual(new Date('2020-05-15T16:01:45.000Z'));
+  expect(RelativeDateHelper.date({ i: '+5' }, { date })).toStrictEqual(new Date('2020-05-15T16:37:45.000Z'));
+  expect(RelativeDateHelper.date({ i: '-5' }, { date })).toStrictEqual(new Date('2020-05-15T16:27:45.000Z'));
+  expect(RelativeDateHelper.date({ i: '+65' }, { date })).toStrictEqual(new Date('2020-05-15T17:37:45.000Z'));
+  expect(RelativeDateHelper.date({ i: '-65' }, { date })).toStrictEqual(new Date('2020-05-15T15:27:45.000Z'));
+
+  expect(RelativeDateHelper.date({ s: '1' }, { date })).toStrictEqual(new Date('2020-05-15T16:32:01.000Z'));
+  expect(RelativeDateHelper.date({ s: '+5' }, { date })).toStrictEqual(new Date('2020-05-15T16:32:50.000Z'));
+  expect(RelativeDateHelper.date({ s: '-5' }, { date })).toStrictEqual(new Date('2020-05-15T16:32:40.000Z'));
+  expect(RelativeDateHelper.date({ s: '+65' }, { date })).toStrictEqual(new Date('2020-05-15T16:33:50.000Z'));
+  expect(RelativeDateHelper.date({ s: '-65' }, { date })).toStrictEqual(new Date('2020-05-15T16:31:40.000Z'));
+
+  expect(RelativeDateHelper.date({ w: '0' }, { date })).toStrictEqual(new Date('2020-05-17T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '1' }, { date })).toStrictEqual(new Date('2020-05-11T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '2' }, { date })).toStrictEqual(new Date('2020-05-12T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '3' }, { date })).toStrictEqual(new Date('2020-05-13T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '4' }, { date })).toStrictEqual(new Date('2020-05-14T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '5' }, { date })).toStrictEqual(new Date('2020-05-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '6' }, { date })).toStrictEqual(new Date('2020-05-16T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '7' }, { date })).toStrictEqual(new Date('2020-05-17T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '8' }, { date })).toStrictEqual(new Date('2020-05-11T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '14' }, { date })).toStrictEqual(new Date('2020-05-17T16:32:45.000Z'));
+
+  expect(RelativeDateHelper.date({ w: '+5' }, { date })).toStrictEqual(new Date('2020-05-20T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '-5' }, { date })).toStrictEqual(new Date('2020-05-10T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '+31' }, { date })).toStrictEqual(new Date('2020-06-15T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ w: '-30' }, { date })).toStrictEqual(new Date('2020-04-15T16:32:45.000Z'));
+
+  expect(RelativeDateHelper.date({ d: '+7', w: '1' }, { date })).toStrictEqual(new Date('2020-05-18T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ d: '+14', w: '1' }, { date })).toStrictEqual(new Date('2020-05-25T16:32:45.000Z'));
+  expect(RelativeDateHelper.date({ d: '-7', w: '1' }, { date })).toStrictEqual(new Date('2020-05-04T16:32:45.000Z')); //*/
+});
+
+test('RelativeDateHelper.description', () => {
+  expect(RelativeDateHelper.description()).toBe('Текущее время;');
+  expect(RelativeDateHelper.description({})).toBe('Текущее время;');
+  expect(RelativeDateHelper.description({ w: '0' })).toBe('День недели: Вс;');
+  expect(RelativeDateHelper.description({ w: '1' })).toBe('День недели: Пн;');
+  expect(RelativeDateHelper.description({ w: '2' })).toBe('День недели: Вт;');
+  expect(RelativeDateHelper.description({ w: '3' })).toBe('День недели: Ср;');
+  expect(RelativeDateHelper.description({ w: '4' })).toBe('День недели: Чт;');
+  expect(RelativeDateHelper.description({ w: '5' })).toBe('День недели: Пт;');
+  expect(RelativeDateHelper.description({ w: '6' })).toBe('День недели: Сб;');
+  expect(RelativeDateHelper.description({ w: '7' })).toBe('День недели: Вс;');
+  expect(RelativeDateHelper.description({ w: '8' })).toBe('День недели: Пн;');
+  expect(RelativeDateHelper.description({ w: '14' })).toBe('День недели: Вс;');
+  expect(RelativeDateHelper.description({ w: '+1' })).toBe('День недели: +1;');
+  expect(RelativeDateHelper.description({ d: '+1', w: '+1' })).toBe('День: +1; День недели: +1;');
+  expect(RelativeDateHelper.description({ y: '+1', m: '+2', d: '+3', h: '+4', i: '+5', s: '+6', w: '1' })).toBe('Год: +1; Месяц: +2; День: +3; Час: +4; Минута: +5; Секунда: +6; День недели: Пн;');
+  expect(RelativeDateHelper.description({ y: '-1', m: '-2', d: '-3', h: '-4', i: '-5', s: '-6', w: '1' })).toBe('Год: -1; Месяц: -2; День: -3; Час: -4; Минута: -5; Секунда: -6; День недели: Пн;');
+});
---
+++ src/helper/RelativeDateHelper.ts
@@ -0,0 +1,138 @@
+/**
+ * Объект для работы с относительной датой.
+ * Правила написания смещений
+ * -1 - минус 1
+ * +1 - плюс 1
+ * 1 - абсолютное значение
+ * если пропущено, то текущее значение
+ * Пример:
+ * { i: "15", i: "0", s: "0" } - 15:00 текущего дня
+ * { d: "-1", h: "15", i: "0", s: "0" } - 15:00 вчера
+ * { d: "+1", h: "15", i: "10", s: "0" } - 15:10 завтра
+ * { d: "+1", h: "15", i: "+10", s: "0" } - завтра 15 часов +10 минут
+ * { d: "-14", w: "1" } - Два понедельника назад
+ */
+export interface RelativeDate {
+  y?: string; // year
+  m?: string; // month
+  d?: string; // day
+  h?: string; // hour
+  i?: string; // minute
+  s?: string; // second
+  w?: string; // week день недели, 1 - понедельник, 7 или 0 - воскресенье
+}
+
+export class RelativeDateHelper {
+  public static i18n = {
+    y: 'Год',
+    m: 'Месяц',
+    d: 'День',
+    h: 'Час',
+    i: 'Минута',
+    s: 'Секунда',
+    dofA: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+    dof: 'День недели',
+    ct: 'Текущее время',
+  };
+
+  public static date(v?: RelativeDate, options?: { date?: Date, withMs?: boolean }): Date {
+    const r = options?.date ? new Date(options.date) : new Date();
+    if (!options?.withMs) {
+      const time = r.getTime();
+      r.setTime(time - time % 1000);
+    }
+    const v2: RelativeDate = {
+      y: v?.y ? `${v.y}` : undefined,
+      m: v?.m ? `${v.m}` : undefined,
+      d: v?.d ? `${v.d}` : undefined,
+      h: v?.h ? `${v.h}` : undefined,
+      i: v?.i ? `${v.i}` : undefined,
+      s: v?.s ? `${v.s}` : undefined,
+      w: v?.w ? `${v.w}` : undefined,
+    };
+    if (v2.y) {
+      const n = parseInt(v2.y, 10);
+      if (v2.y.startsWith('+') || v2.y.startsWith('-')) {
+        r.setFullYear(r.getFullYear() + n);
+      } else {
+        r.setUTCFullYear(n);
+      }
+    }
+    if (v2.m) {
+      const n = parseInt(v2.m, 10);
+      if (v2.m.startsWith('+') || v2.m.startsWith('-')) {
+        if (n >= 12) r.setFullYear(r.getFullYear() + Math.floor(n / 12));
+        if (n <= -12) r.setFullYear(r.getFullYear() - Math.floor((-1 * n) / 12));
+        r.setMonth(r.getMonth() + n % 12);
+      } else {
+        r.setMonth(n - 1);
+      }
+    }
+    if (v2.d) {
+      const n = parseInt(v2.d, 10);
+      if (v2.d.startsWith('+') || v2.d.startsWith('-')) {
+        r.setTime(r.getTime() + n * 864e5);
+      } else {
+        r.setDate(n);
+      }
+    }
+    if (v2.h) {
+      const n = parseInt(v2.h, 10);
+      if (v2.h.startsWith('+') || v2.h.startsWith('-')) {
+        r.setTime(r.getTime() + n * 36e5);
+      } else {
+        r.setHours(n);
+      }
+    }
+    if (v2.i) {
+      const n = parseInt(v2.i, 10);
+      if (v2.i.startsWith('+') || v2.i.startsWith('-')) {
+        r.setTime(r.getTime() + n * 6e4);
+      } else {
+        r.setMinutes(n);
+      }
+    }
+    if (v2.s) {
+      const n = parseInt(v2.s, 10);
+      if (v2.s.startsWith('+') || v2.s.startsWith('-')) {
+        r.setTime(r.getTime() + n * 1e3);
+      } else {
+        r.setSeconds(n);
+      }
+    }
+    if (v2.w) {
+      let n = parseInt(v2.w, 10);
+      if (v2.w.startsWith('+') || v2.w.startsWith('-')) {
+        r.setTime(r.getTime() + n * 864e5);
+      } else {
+        n = n % 7;
+        if (n === 0) n = 7; // воскресение должно быть 7, а не 0
+        const w = (n - r.getDay());
+        r.setTime(r.getTime() + w * 864e5);
+      }
+    }
+    return r;
+  }
+
+  public static description(v?: RelativeDate) {
+    const r: string[] = [];
+    if (v?.y) r.push(`${RelativeDateHelper.i18n.y}: ${v.y}`);
+    if (v?.m) r.push(`${RelativeDateHelper.i18n.m}: ${v.m}`);
+    if (v?.d) r.push(`${RelativeDateHelper.i18n.d}: ${v.d}`);
+    if (v?.h) r.push(`${RelativeDateHelper.i18n.h}: ${v.h}`);
+    if (v?.i) r.push(`${RelativeDateHelper.i18n.i}: ${v.i}`);
+    if (v?.s) r.push(`${RelativeDateHelper.i18n.s}: ${v.s}`);
+    if (v?.w) {
+      let n = parseInt(v.w, 10);
+      if (v.w.startsWith('+') || v.w.startsWith('-')) {
+        r.push(`${RelativeDateHelper.i18n.dof}: ${v.w}`);
+      } else {
+        n = n % 7;
+        if (n === 0) n = 7; // воскресение должно быть 7, а не 0
+        r.push(`${RelativeDateHelper.i18n.dof}: ${RelativeDateHelper.i18n.dofA[n - 1]}`);
+      }
+    }
+    if (r.length === 0) return `${RelativeDateHelper.i18n.ct};`;
+    return `${r.join('; ')};`;
+  }
+}
